53a02125a54917e9093ac73b8d12add5b1a05cc8
Cherry1 <cherry2022automation@gmail.com>
2022-07-14 17:34:11 +0900

    関数化、外部使用可に

--- trimming.py
+++ trimming.py
@@ -5,6 +5,8 @@ import cv2
 import os
 import cherry
 
+# 変更を行う変数 ========================================================================
+
 # トリミング元ディレクトリ
 open_dir = cherry.cherry.cherry_picture_directory
 
@@ -13,8 +15,17 @@ output_dir = "C:\\Users\\cherr\\Desktop\\data\\cherry_photo\\trimming_2500\\"
 
 trm_size = 2500
 
-failed_num = []
+hsv_1_min = [0, 80, 30]
+hsv_1_max = [15, 255, 255]
+hsv_2_min = [160, 80, 30]
+hsv_2_max = [179, 255, 255]
 
+area_filter_min = 1000000
+area_filter_max = 3000000
+
+# ======================================================================================
+
+failed_num = []
 
 # ディレクトリ内に存在する画像のシリアル番号をリスト化
 def get_serial_nums(files):
@@ -30,47 +41,51 @@ def get_serial_nums(files):
             pass
     return serial_nums
 
+def trim():
 
-# ディレクトリ内のファイル名を取得
-open_files = os.listdir(open_dir)
-already_files = os.listdir(output_dir)
+    # ディレクトリ内のファイル名を取得
+    open_files = os.listdir(open_dir)
+    already_files = os.listdir(output_dir)
 
-# ディレクトリ内の画像のシリアル番号リストを取得
-open_serial_num = get_serial_nums(open_files)
-already_serial_num = get_serial_nums(already_files)
+    # ディレクトリ内の画像のシリアル番号リストを取得
+    open_serial_num = get_serial_nums(open_files)
+    already_serial_num = get_serial_nums(already_files)
 
-# 出力先に存在しない画像のみトリミングして保存
-for num in open_serial_num:
+    # 出力先に存在しない画像のみトリミングして保存
+    for num in open_serial_num:
 
-    if num in already_serial_num:
-        continue
-
-    try:
-
-        cherry_01 = cherry.cherry(num)
-
-        if cherry_01.enable == False:
+        if num in already_serial_num:
             continue
 
-        # さくらんぼ検出
-        cherry_01.cherry_detection()
+        try:
 
-        # トリミング
-        cherry_01.trimming(trm_size)
+            cherry_01 = cherry.cherry(num, picture_dir=open_dir)
+
+            if cherry_01.enable == False:
+                continue
+
+            # さくらんぼ検出
+            cherry_01.cherry_detection(hsv_1_min=hsv_1_min, hsv_1_max=hsv_1_max, hsv_2_min=hsv_2_min, hsv_2_max=hsv_2_max, area_filter_min=area_filter_min, area_filter_max=area_filter_max)
+
+            # トリミング
+            cherry_01.trimming(trm_size)
+            
+            for dir in cherry_01.pictures:
+                cv2.imwrite(output_dir + cherry_01.file_name + "_" + dir + ".jpeg", cherry_01.pictures[dir].trim_img)
+            
+            print(str(num) + "done")
+
+        except:
+            failed_num.append(num)
+            print(str(num) + "failed")
+
+    print("------------------------------")
+    print("Processing completed")
+    print("")
+    print("failed number")
+    print(failed_num)
+    print("------------------------------")
         
-        for dir in cherry_01.pictures:
-            cv2.imwrite(output_dir + cherry_01.file_name + "_" + dir + ".jpeg", cherry_01.pictures[dir].trim_img)
-        
-        print(str(num) + "done")
+if __name__ == '__main__':
 
-    except:
-        failed_num.append(num)
-        print(str(num) + "failed")
-
-print("------------------------------")
-print("Processing completed")
-print("")
-print("failed number")
-print(failed_num)
-print("------------------------------")
-    
+    trim()
