f5a7c02762879c79f2d8fe40f57b1ff473d4e1fd
Cherry1 <cherry2022automation@gmail.com>
2022-07-25 16:31:40 +0900

    first commit

---
+++ resize_025_trimming_640.py
@@ -0,0 +1,42 @@
+import resize
+import trimming
+import cherry
+
+# リサイズ元ディレクトリ
+resize.open_dir = "C:\\Users\\cherr\\Desktop\\data\\cherry_photo\\original\\"
+
+# 出力先ディレクトリ
+resize.output_dir = "C:\\Users\\cherr\\Desktop\\data\\cherry_photo\\resize_025\\"
+
+# 拡大率
+resize.magnification = 0.25
+
+# トリミング元ディレクトリ
+trimming.open_dir = resize.output_dir
+
+# 出力先ディレクトリ
+trimming.output_dir = "C:\\Users\\cherr\\Desktop\\data\\cherry_photo\\resize_025_trim_640\\"
+
+trimming.trm_size = 640
+
+trimming.hsv_1_min = [0, 80, 10]
+trimming.hsv_1_max = [30, 255, 255]
+trimming.hsv_2_min = [160, 80, 10]
+trimming.hsv_2_max = [179, 255, 255]
+
+trimming.area_filter_min = 20000
+trimming.area_filter_max = 250000
+
+
+resize_failed = resize.resize()
+trim_failed = trimming.trim()
+
+print("---------------------------------")
+
+print("")
+print("resize_failed")
+print(resize_failed)
+
+print("")
+print("trim_failed")
+print(trim_failed)
